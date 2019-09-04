27a2913c8504543ab4517406221f84138bb12ead
petroborys <vagrant@vagrant.vm>
2019-09-04 17:24:43 +0000

    ip in main.c

--- src/main.c
+++ src/main.c
@@ -81,7 +81,7 @@ int main(int argc, char *argv[])
 	config_init(&cfg);
 
 	/* Read the file. If there is an error, report it and exit. */
-	if(! config_read_file(&cfg, "ts_module.cfg"))
+	if(! config_read_file(&cfg, "/tmp/ts_module.cfg"))
 	{
 	fprintf(stderr, "%s:%d - %s\n", config_error_file(&cfg),
 	        config_error_line(&cfg), config_error_text(&cfg));
